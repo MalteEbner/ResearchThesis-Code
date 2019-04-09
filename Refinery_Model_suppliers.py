5b0b09a692aec081473a0bb0aa29c8dec283a487
Metlam <malte.ebner@gmail.com>
2019-04-09 17:18:47 +0200

    reading supplier from excel

--- Refinery_Model_suppliers.py
+++ Refinery_Model_suppliers.py
@@ -1,9 +1,39 @@
-import Meta-Model.py
+import Meta_Model
 
 
 '''
-define Suppliers
+load data from excel
 '''
-def getSuppliers():
-    suppliers = []
-    suppliers += 
+import pandas as pd
+
+def loadSupplierData(filename='/Users/malteebner/Library/Mobile Documents/com~apple~CloudDocs/Master ETIT/10. Semester/Forschungsarbeit/Project management paper/SimGame translated - Andre Heleno.xlsx'):
+    sheet = pd.read_excel(filename, sheet_name='Suppliers')
+    #print(sheet)
+
+    competenceTypes = []
+    for competenceIndex in range(10):
+        columnLetter = round(4 + competenceIndex * 3)
+        type = sheet[int(columnLetter)][int(0)]
+        competenceTypes.append(type)
+
+    suppliers =[]
+    for supplierIndex in range(11):
+        rowNumber = 2 + supplierIndex
+        supplierName = sheet[3][rowNumber]
+        competences = []
+        for competenceIndex in range(10):
+            columnLetter = 4 + competenceIndex * 3
+            duration = sheet[columnLetter][rowNumber]
+            if duration > 0: #only add competence if supplier has it
+                cost = sheet[columnLetter+1][rowNumber]
+                quality = sheet[columnLetter + 2][rowNumber]
+                competence = Meta_Model.Compentence(competenceTypes[competenceIndex],duration,cost,quality)
+                competences.append(competence)
+        supplier = Meta_Model.Supplier(supplierName,competences)
+        suppliers.append(supplier)
+
+    return suppliers
+
+
+suppliers = loadSupplierData()
+print(suppliers)
