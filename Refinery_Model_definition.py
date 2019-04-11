3a00f7b9f2d8a720dd318dd14d889991eaf97c58
Metlam <malte.ebner@gmail.com>
2019-04-11 09:10:22 +0200

    file to define model and first tests

--- Refinery_Model_definition.py
+++ Refinery_Model_definition.py
@@ -1,1 +1,13 @@
+import Refinery_Model_ProjectNetwork
+import Refinery_Model_suppliers
+
+suppliers = Refinery_Model_suppliers.loadSupplierData()
+activities = Refinery_Model_ProjectNetwork.loadActivityData(suppliers)
+
+
+for activity in activities:
+    blub = activity.variants[0].simulate()
+    print(blub)
+
+
 
