9ce788d7543e50fc197a959b18c7273bb3c97ccf
Metlam <malte.ebner@gmail.com>
2019-04-15 18:04:14 +0200

    bugfix: activities with correct IDs

--- Refinery_Model_ProjectNetwork.py
+++ Refinery_Model_ProjectNetwork.py
@@ -29,7 +29,7 @@ def loadActivityData(allSuppliers, filename='/Users/malteebner/Library/Mobile Do
             base_duration = sheet[6][activityIndex]
             base_cost_per_day = sheet[8][activityIndex]
             suppliers = [supplier for supplier in allSuppliers if supplier.hasCompetenceType(activityType)]
-            activity = Meta_Model.Activity_refinery(predecessorIndices,suppliers,activityType,base_duration,base_cost_per_day,name,activityIndex)
+            activity = Meta_Model.Activity_refinery(predecessorIndices,suppliers,activityType,base_duration,base_cost_per_day,name,activityNumber)
             activities.append(activity)
 
     #replace predecessor indices by class instances
