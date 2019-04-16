a02002b371fb2e4811b61856eaa4653595e6da8c
Metlam <malte.ebner@gmail.com>
2019-04-16 14:22:57 +0200

    Refinery_Model has additional functions

--- Model/Refinery_Model_definition.py
+++ Model/Refinery_Model_definition.py
@@ -2,7 +2,7 @@ from Model import Refinery_Model_suppliers, Refinery_Model_ProjectNetwork
 import numpy as np
 
 
-class Model_Refinery:
+class Model_Refinery():
     def __init__(self):
         self.suppliers = Refinery_Model_suppliers.loadSupplierData()
         self.activities = Refinery_Model_ProjectNetwork.loadActivityData(self.suppliers)
@@ -21,6 +21,12 @@ class Model_Refinery:
         self.averageQuality = sum(act.quality * act.base_duration for act in self.activities)/sum(act.base_duration for act in self.activities)
         return self.totalDuration, self.totalCost, self.averageQuality
 
+    def simulate_returnLoss(self,chosenVariantIndizes,lossFunction=[]):
+        if lossFunction == []:
+            lossFunction = self.defaultLossFunction
+        loss = lossFunction(self.simulate(chosenVariantIndizes))
+        return loss
+
     def getStartpoint(self,lossFunction=[]):
         if lossFunction == []:
             lossFunction = self.defaultLossFunction
@@ -31,6 +37,16 @@ class Model_Refinery:
             activity.variants[bestVariant].simulate() #assume all predecessors(and their quality) are optimal
             startIndizes.append(bestVariant)
         return startIndizes
+
+    def getActivityLosses(self,lossFunction=[]):
+        if lossFunction == []:
+            lossFunction = self.defaultLossFunction
+        activityLosses = []
+        for activity in self.activities:
+            variantLosses = [lossFunction(var.simulate()) for var in activity.variants]
+            activityLosses.append(variantLosses)
+        return activityLosses
+
 
     def getOptFunction(self, lossFunction=[]):
         if lossFunction == []:
@@ -45,7 +61,7 @@ class Model_Refinery:
         return lines
 
 
-
+'''
 model = Model_Refinery()
 print("\nModel 1:")
 print(model.simulate())
@@ -54,7 +70,7 @@ print("\nModel 2:")
 startpoint = model.getStartpoint()
 print(model.simulate(startpoint))
 print(model)
-
+'''
 
 
 
