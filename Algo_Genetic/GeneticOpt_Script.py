637580d0f5654b8029968f47a15bc47e60564d55
Metlam <malte.ebner@gmail.com>
2019-05-18 01:12:18 +0200

    more informative geneticOpt script

--- Algo_Genetic/GeneticOpt_Script.py
+++ Algo_Genetic/GeneticOpt_Script.py
@@ -1,21 +1,29 @@
 from Algo_Genetic import GeneticOpt_Class
+import time
 
 from Model_Refinery import Refinery_Model
 from Model_TopSim_RollerCoaster import RollerCoaster_Model
 from Model_MIS import MIS_Model
 
-#model = RollerCoaster_Model.Model_RollerCoaster()
+#model = RollerCoaster_Model.Model_RollerCoaster(False)
 #model = Refinery_Model.Model_Refinery()
 model = MIS_Model.Model_MIS()
+print(model.calcPerformanceFunction())
 
 activityVariantNumbers = model.getVariantNumbers()
-genePool = GeneticOpt_Class.GeneticOpt(activityVariantNumbers,model.simulate_returnLoss,5000)
-startChromosome = model.getZeroStartpoint()
+genePool = GeneticOpt_Class.GeneticOpt(activityVariantNumbers,model.simulate_returnLoss,100)
+if hasattr(model,'getGoodStartpoint'):
+    startChromosome = model.getGoodStartpoint()
+else:
+    startChromosome = model.getZeroStartpoint()
 print(model.simulateMean(startChromosome))
 genePool.population.append(startChromosome)
+start = time.time()
 for i in range(100):
     best = genePool.generateNewPop()
     print(str(i) + ":  " + str(model.simulateMean(best)))
+end = time.time()
+print('time needed: ' + str(end-start))
 
 
 best = genePool.getBestPop(model.simulateMean_returnLoss)
