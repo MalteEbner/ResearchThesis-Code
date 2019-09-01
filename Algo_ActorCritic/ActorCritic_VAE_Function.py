fba19bfad168c0d98a2109a96cd19e88493a0ecf
Metlam <malte.ebner@gmail.com>
2019-09-01 12:43:06 +0200

    show losses better

--- Algo_ActorCritic/ActorCritic_VAE_Function.py
+++ Algo_ActorCritic/ActorCritic_VAE_Function.py
@@ -31,16 +31,16 @@ def actorCritic_VAE_RunAlgo(model=0, verbose = 2, hyperparams=0):
         print('baseline:' + str(baseline) + " baseline_std: " + str(baseStd))
 
     '''hyperparams'''
-    batchSize = hyperparams.get('batchSize',16)
+    batchSize = hyperparams.get('batchSize',64)
     noSamples = hyperparams.get('noSamples',80000)
     noIterations = int(noSamples/batchSize)
-    verboseNoIterations = max(int(64/batchSize),1)
+    verboseNoIterations = max(int(1024/batchSize),1)
     baselineUpdateFactor = hyperparams.get('baselineUpdateFactor',0.1)
 
     explorationFactor = hyperparams.get('explorationFactor',0.9)
     explorationDecayFactor = hyperparams.get('explorationDecayFactor',0.99)
 
-    learningRate = hyperparams.get('learningRate',0.0003)
+    learningRate = hyperparams.get('learningRate',0.009)
     learningRateDecayFactor = hyperparams.get('learningRateDecayFactor',0.98)
 
     '''run actor-critic algorithm'''
@@ -66,7 +66,7 @@ def actorCritic_VAE_RunAlgo(model=0, verbose = 2, hyperparams=0):
 
         #print best loss
         if  verbose >= 2 and (i % verboseNoIterations == 0 or i == noIterations - 1):
-            loss = meanLoss
+            loss = np.min(losses)
             if np.isnan(loss):
                 print('Loss is nan, stopping algorithm')
                 break
