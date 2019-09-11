8a0510294e6f79d59c41e0aed861abc8b446b6e7
Metlam <malte.ebner@gmail.com>
2019-09-11 18:05:08 +0200

    bayesopt_hyperopt now working with new action spac

--- Algo_BayesOpt_hyperopt/BayesOpt_Hyperopt.py
+++ Algo_BayesOpt_hyperopt/BayesOpt_Hyperopt.py
@@ -8,7 +8,7 @@ from Model_Refinery import Refinery_Model
 from Model_TopSim_RollerCoaster import RollerCoaster_Model
 from Model_MIS import MIS_Model
 from Model_general import commonFunctions
-
+from gym import spaces
 
 
 '''generate Model with its options'''
@@ -22,46 +22,42 @@ model = generateModel(modelOptions)
 define search spaces
 '''
 actionSpace = model.getActionSpace()
-if actionSpace.withScheduleCompression:
-    raise NotImplementedError
 
-noActivityVariants = actionSpace.VariantNumbers()
-space_equalChoice = {}
-for num in range(len(noActivityVariants)):
-    varName = "act_ID_"+str(num)
-    noVariants = noActivityVariants[num]
-    space_equalChoice[varName] = hp.choice(varName,range(0,noVariants))
-
-space_softmaxChoice = {}
-activityLosses = model.getActivityLosses()
-for num in range(len(noActivityVariants)):
-    varName = "act_ID_" + str(num)
-    noVariants = noActivityVariants[num]
-    variantLosses = activityLosses[num]
-    variantProbabilities = commonFunctions.probsFromLosses(variantLosses)
-    choices = zip(variantProbabilities,range(0,noVariants))
-    space_softmaxChoice[varName] = hp.pchoice(varName,choices)
+searchSpace = {}
+varNumber = 0
+for space in actionSpace.spaces:
+    if isinstance(space, spaces.MultiDiscrete):
+        for index, noVariants in enumerate(space.nvec):
+            varName = str(varNumber)
+            param = hp.choice(varName,range(0,noVariants))
+            searchSpace[varName] = param
+            varNumber+=1
+    elif isinstance(space, spaces.Box):
+        for index in range(space.shape[0]):
+            varName = varName = str(varNumber)
+            param = hp.uniform(varName, 0.5, 1)
+            searchSpace[varName] = param
+            varNumber += 1
+    else:
+        raise NotImplementedError
 
 '''
-translate chosenVariantIndizes as dictionary into chosenVariantIndizes as list of integers
+objective Function with variable translation
 '''
-def get_chosenVariantIndizes(chosenVariantIndizes_dict):
-    chosenVariantIndizes = []
-    for num in range(len(noActivityVariants)):
-        varName = "act_ID_" + str(num)
-        chosenVariantID = int(chosenVariantIndizes_dict[varName])
-        chosenVariantIndizes.append(chosenVariantID)
-    return chosenVariantIndizes
 
-def objectiveFunction(chosenVariantIndizes_dict):
+
+def objectiveFunction(varDict):
+    vars = [varDict[str(key)] for key in sorted(varDict.keys(),key = int)]
     action = ActionSpace.Action(actionSpace)
-    action.saveIndizesCombined(get_chosenVariantIndizes(chosenVariantIndizes_dict))
+    action.saveEverythingCombined(vars)
+    if not action.checkIfValuesInRange():
+        i=0
     loss = model.simulate_returnLoss(action)
     return loss
 
 '''
 do the optimization
 '''
-best = fmin(objectiveFunction,space_softmaxChoice,algo=tpe.suggest,max_evals=400)
+best = fmin(objectiveFunction,searchSpace,algo=tpe.suggest,max_evals=400)
 
 print("Best: " + str(objectiveFunction(best)))
