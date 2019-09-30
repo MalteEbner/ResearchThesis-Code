d1f00b48d6b68d223216b9188992fee97155187d
MalteEbner <malte.ebner@gmail.com>
2019-09-30 17:30:59 +0200

    algo_heuristic now includes zeroAction

--- Algo_Genetic/Algo_heuristic.py
+++ Algo_Genetic/Algo_heuristic.py
@@ -5,12 +5,15 @@ from Interface.Model_options import Model_options
 
 
 '''generate Model with its options'''
-modelOptions = Model_options('MIS ') #type: 'Refinery' , 'MIS' or 'RollerCoaster'
+modelOptions = Model_options('RollerCoaster') #type: 'Refinery' , 'MIS' or 'RollerCoaster'
 modelOptions.probabilistic = False
 modelOptions.withScheduleCompression=False
 model = generateModel(modelOptions)
 
 heuristicAction = model.projectModel.getHeuristicBestAction()
-print("action: " + str(heuristicAction))
-performance = model.projectModel.simulateMean(heuristicAction)
+zeroAction = model.projectModel.getZeroAction()
+action = zeroAction
+#action = heuristicAction
+print("action: " + str(action))
+performance = model.projectModel.simulateMean(action)
 print('heuristic performance: ' + str(performance))
