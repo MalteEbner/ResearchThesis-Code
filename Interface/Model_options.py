39d3e269c5fdbf3dfcd27b88a8d6eb08aeefefc0
MalteEbner <malte.ebner@gmail.com>
2019-09-30 16:54:43 +0200

    renamed .h5 files to make old ones clear

--- Interface/Model_options.py
+++ Interface/Model_options.py
@@ -28,7 +28,7 @@ class Model_options():
         if self.withEvents:
             name += '_withEvents'
         if self.withScheduleCompression:
-            name += '_withScheduleCompression'
+            name += '_withScheduleCompression075'
         name+= '_'+str(latentDim)
         name += '.h5'
         from pathlib import Path
